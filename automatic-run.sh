ba73d22285b23aa67145623e33847f28fed60f28
server-kun <contact.computerbread@gmail.com>
2025-12-02 03:27:04 +0000

    bit better log

--- automatic-run.sh
+++ automatic-run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 source "$HOME/.bashrc"
-echo "starting: "
-date
+echo "============================================================="
+echo "starting: $(date)"
 cd /home/znippets/znippets/
 git pull
+echo "ok"
 zigup 0.16.0-dev.1484+d0ba6642b
 zig build run -Doptimize=ReleaseFast
